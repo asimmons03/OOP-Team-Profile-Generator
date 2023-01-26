70d1125ef197e510e1d9f5e96c8cc1e6fd36a666
Andrew Simmons <asimmons03@yahoo.com>
2023-01-25 23:02:58 -0600

    initial code

--- team.js
+++ team.js
@@ -1,1 +1,30 @@
+const inquirer = require('inquirer');
 
+inquirer  
+    .prompt([
+        {
+            type: 'input',
+            message: "What is the manager's name?",
+            name: 'manager'
+        },
+        {
+            type: 'input',
+            message: "What is the manager's email?",
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: "What is the manager's name?",
+            name: 'manager'
+        },
+        {
+            type: 'input',
+            message: "What is the manager's name?",
+            name: 'manager'
+        },
+
+    ])
+    .then((answers) => {
+        console.log(answers)
+        console.log(answers.manager)
+    })
